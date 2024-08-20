406023c3a5cc8a5f10db6617e4403e49f28c3fad
Fernando Arcega <senaifernandoquadros@gmail.com>
2024-08-20 20:58:30 -0300

    Add files via upload

---
+++ EXERCICÍO.01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.Intrinsics.X86;
+
+namespace ConsultaEmArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                    //1.CONSULTAS EM ARRAYS
+            //Exercício: Crie um array de números inteiros. Use LINQ para encontrar todos
+                        //os números pares e ordená-los em ordem crescente.
+
+            // Array aplicado
+            int[] numeros = { 10, 21, 4, 15, 8, 33, 2, 14 };
+
+            // Usando o LINQ
+            var numerosParesOrdenados = numeros.Where(n => n % 2 == 0).OrderBy(n => n);
+
+            // Números pares ordenados
+            Console.WriteLine("\n\n\n           Números pares ordenados em crescente:");
+            foreach (var                                numero in numerosParesOrdenados)
+            {
+                Console.WriteLine(                          numero);
+            }
+
+            // Pausa do console aberto
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ EXERCICÍO.02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsultaEmListas
+{
+    class Program
+    {
+                                    //2. CONSULTAS EM LISTAS
+                //Crie uma lista de strings representando nomes de pessoas.Use
+                    //LINQ para encontrar nomes que comecem com a letra "A"
+                    //e ordene-os em ordem decrescente.
+
+
+        static void Main(string[] args)
+        {
+            // Lista de strings representando nomes de pessoas
+            List<string> nomes = new List<string> { "\n\n\n         Alice", "André", "Bruno", "Ana", "Carlos", "Amanda" };
+
+            // Uso do LINQ para encontrar nomes que começam com "A"
+            var nomesComAOrdenados = nomes.Where(n => n.StartsWith("A")).OrderByDescending(n => n);
+
+            // Nomes que começam com a letra "A" em ordem decrescente
+            Console.WriteLine("\n\n\n                           Nomes que começam com 'A' ordenados em ordem decrescente:");
+            foreach (var nome in nomesComAOrdenados)
+            {
+                Console.WriteLine(nome);
+            }
+
+            // Pausa para manter o console aberto
+            Console.ReadLine();
+        }
+    }
+}
+
